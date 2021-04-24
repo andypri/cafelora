59cb09ca27f258e2938dcaa596cf6d065fa97e1c
Andrea Příhoda <andyprihoda@gmail.com>
2021-04-24 11:45:53 +0200

    nav buttons display and hide

--- src/index.js
+++ src/index.js
@@ -1,3 +1,17 @@
 import './style.css';
 
-console.log('funguju!');
+// console.log('funguju!');
+
+const navBtnElm = document.querySelector('#nav__btn');
+const navElm = document.querySelector('#nav');
+const navItems = document.querySelectorAll('#nav__item');
+
+navBtnElm.addEventListener('click', () => {
+  navElm.classList.toggle('nav-closed');
+});
+
+navItems.forEach((navItem) => {
+  navItem.addEventListener('click', () => {
+    navElm.classList.toggle('nav-closed');
+  });
+});
