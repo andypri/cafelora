334766a7d6adcfcf9e41855de85a56ad490c267b
Andrea Příhoda <andyprihoda@gmail.com>
2021-04-24 12:19:54 +0200

    menu button: classes and text

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import './style.css';
 
 // console.log('funguju!');
 
+// 3.
 const navBtnElm = document.querySelector('#nav__btn');
 const navElm = document.querySelector('#nav');
 const navItems = document.querySelectorAll('#nav__item');
@@ -14,4 +15,21 @@ navItems.forEach((navItem) => {
   navItem.addEventListener('click', () => {
     navElm.classList.toggle('nav-closed');
   });
+});
+
+// 4.
+const orderBtnElm = document.querySelector('#order-btn');
+const drinkCupElm = document.querySelector('#drink__cup');
+let ordered = false;
+
+orderBtnElm.addEventListener('click', () => {
+  if (ordered === false) {
+    drinkCupElm.classList.add('drink__cup--selected');
+    orderBtnElm.textContent = 'Zrušit';
+    ordered = true;
+  } else {
+    drinkCupElm.classList.remove('drink__cup--selected');
+    orderBtnElm.textContent = 'Objednat';
+    ordered = false;
+  }
 });
